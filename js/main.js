c6225a9e23009f8a37cc64e67a5f76f013df2291
hpknollenberg <hpknollenberg@gmail.com>
2024-03-26 11:57:32 -0400

    Added funcitonal Darken/Lighten button.

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ const pictures = [{name: "img/pic1.jpg", alt: "Closeup of a human eye"},
                 {name: "img/pic2.jpg", alt: "Abstract swirly lines of light brown and grayish blues"},
                 {name: "img/pic3.jpg", alt: "Purple and white flowers"},
                 {name: "img/pic4.jpg", alt: "Hieroglyphics"},
-                {name: "img/pic5.jpg", alt: "Brown butterfly on a green leaf"}]
+                {name: "img/pic5.jpg", alt: "Brown butterfly on a green leaf"}];
 
-//Looping through images
+//Looping through images, listening for click
 for (i = 0; i <= 4; i++){
     const newImage = document.createElement('img');
     newImage.setAttribute('src', pictures[i].name);
@@ -25,5 +25,15 @@ for (i = 0; i <= 4; i++){
     });
 }
 
-
 //Wiring up the Darken/Lighten button
+btn.addEventListener("click", () => {
+    if (btn.getAttribute("class") === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
